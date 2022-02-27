cb104cb015317af403426d4091d008927f912a1d
Jake Weinstein <jake@aospa.co>
2022-02-27 06:36:19 +0000

    Introduce Cortex-A510 CPU variant
    Test: llvm-objdump -d
    Change-Id: Ifb97ab84b441598feeb75860ac3265d79257714d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- cc/config/arm64_device.go
+++ cc/config/arm64_device.go
@@ -56,6 +56,9 @@ var (
 	arm64Cppflags = []string{}
 
 	arm64CpuVariantCflags = map[string][]string{
+		"cortex-a510": []string{
+			"-mcpu=cortex-a510",
+		},
 		"cortex-a53": []string{
 			"-mcpu=cortex-a53",
 		},
@@ -107,6 +110,7 @@ func init() {
 		pctx.StaticVariable("Arm64"+variant+"VariantCflags", strings.Join(cflags, " "))
 	}
 
+	pctx.StaticVariable("Arm64CortexA510Cflags", strings.Join(arm64CpuVariantCflags["cortex-a510"], " "))
 	pctx.StaticVariable("Arm64CortexA53Cflags", strings.Join(arm64CpuVariantCflags["cortex-a53"], " "))
 	pctx.StaticVariable("Arm64CortexA55Cflags", strings.Join(arm64CpuVariantCflags["cortex-a55"], " "))
 	pctx.StaticVariable("Arm64CortexA76Cflags", strings.Join(arm64CpuVariantCflags["cortex-a76"], " "))
@@ -119,6 +123,7 @@ func init() {
 
 var (
 	arm64CpuVariantCflagsVar = map[string]string{
+		"cortex-a510": "${config.Arm64CortexA510Cflags}",
 		"cortex-a53": "${config.Arm64CortexA53Cflags}",
 		"cortex-a55": "${config.Arm64CortexA55Cflags}",
 		"cortex-a72": "${config.Arm64CortexA53Cflags}",
